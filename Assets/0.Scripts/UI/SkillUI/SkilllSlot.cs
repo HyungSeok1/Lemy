f8e8aa5894d96e8ffa676ada0cfe69242c504e4d
jaeyongpark121 <jaeyongpark01210121@gmail.com>
2025-09-08 08:50:44 +0900

    feat: skillSwapUI 수정 (물방울 움직임은 구현 아직)

--- Assets/0.Scripts/UI/SkillUI/SkilllSlot.cs
+++ Assets/0.Scripts/UI/SkillUI/SkilllSlot.cs
@@ -1,24 +1,38 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using DG.Tweening; // DoTween 네임스페이스 추가
 
 /// <summary>
-///  새로 만든 스킬 UI
-///  스킬 슬롯 코드
+/// 새로 만든 스킬 UI
+/// 스킬 슬롯 코드
 /// </summary>
-public class SkillSlot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class SkillSlot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler,
+                          IPointerEnterHandler, IPointerExitHandler
 {
     private Canvas canvas;
     private GameObject dragClone; // 드래그용 복사본
 
-    public ISkill skill; // ...스킬 데이터만 있으면 될줄 알았는데 아니어서 새로 만듦
+    public ISkill skill;
     public Image skillImage;
 
+    private Vector3 originalScale; // 원래 스케일 저장
 
     private void Awake()
     {
         canvas = GetComponentInParent<Canvas>();
-        dragClone = GetComponent<GameObject>();
+        originalScale = transform.localScale;
+    }
+
+    private void OnEnable()
+    {
+        SkillSwapUI.OnSkillSwapUIClosed += ClearDragClone;
+    }
+
+    private void OnDisable()
+    {
+        SkillSwapUI.OnSkillSwapUIClosed -= ClearDragClone;
+        ClearDragClone(); // 혹시 남아있으면 정리
     }
 
     public void OnBeginDrag(PointerEventData eventData)
@@ -42,7 +56,27 @@ public class SkillSlot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        ClearDragClone();
+    }
+
+    private void ClearDragClone()
+    {
         if (dragClone != null)
+        {
             Destroy(dragClone);
+            dragClone = null;
+        }
+    }
+
+    // 마우스가 슬롯에 올라갔을 때
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.DOScale(originalScale * 1.1f, 0.2f).SetEase(Ease.OutBack);
+    }
+
+    // 마우스가 슬롯에서 벗어났을 때
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.DOScale(originalScale, 0.2f).SetEase(Ease.OutBack);
     }
 }
