6ffb0297100fcea116c74107527a36b287b73547
jaeyongpark121 <jaeyongpark01210121@gmail.com>
2025-09-07 22:23:25 +0900

    feat: map3 투명벽 설치, 번개 프리팹 길이 조정

--- Assets/0.Scripts/MapObjects/Structures/LimitedZone/Zones/Map3_test.cs
+++ Assets/0.Scripts/MapObjects/Structures/LimitedZone/Zones/Map3_test.cs
@@ -8,6 +8,8 @@ public class Map3_test : BaseZone
     [SerializeField] private Player player;
     [SerializeField] private Collider2D limitedZoneRange;
     [SerializeField] private GameObject door;
+    [SerializeField] private GameObject invisibleWalls;
+
 
     private void Start()
     {
@@ -15,6 +17,13 @@ public class Map3_test : BaseZone
         if (player == null) {player = Player.Instance;}
         if (limitedZoneRange == null) { limitedZoneRange = transform.Find("zoneRange").GetComponent<Collider2D>(); }
         if (door == null) { door = transform.Find("3_parallax2").gameObject; }
+        if (invisibleWalls == null) 
+        { 
+            invisibleWalls = transform.Find("InvisibleWalls").gameObject; 
+        }
+
+        invisibleWalls.SetActive(false);
+
     }
     private void Update()
     {
@@ -23,6 +32,8 @@ public class Map3_test : BaseZone
 
     protected override IEnumerator OnStart() // 제약구간 시작 부분
     {
+        invisibleWalls.SetActive(true);
+
         StartCoroutine(Spin(door, 3f, 22.5f));
         Debug.LogWarning("Map3: 시작 준비 중");
         yield return new WaitForSeconds(3f);
@@ -32,6 +43,8 @@ public class Map3_test : BaseZone
 
     protected override IEnumerator OnEnd()
     {
+        invisibleWalls.SetActive(false);
+
         Debug.Log("Map3: 종료 처리 중");
         yield return new WaitForSeconds(3f);
         Debug.Log("Map3: 종료");
@@ -41,7 +54,7 @@ public class Map3_test : BaseZone
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            StartCoroutine(Spin(door, 3f, 22.5f));
+            // StartCoroutine(Spin(door, 3f, 22.5f));
             TryExecute();
         }
     }
@@ -50,8 +63,9 @@ public class Map3_test : BaseZone
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            StartCoroutine(Spin(door, 3f, -22.5f));
-            TryExecute();
+            // 구역 나갈때는 따로 뭐 해줄 필요 없음
+            // StartCoroutine(Spin(door, 3f, -22.5f));
+            // TryExecute(); 
         }
     }
 
