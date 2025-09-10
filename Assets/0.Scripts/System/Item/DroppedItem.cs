14ba5d9063de8344f2bc2a2dd2a78e64ae2c90fc
JANGHYUNGSEOK\장형석 <hsjang04@gmail.com>
2025-09-10 22:53:43 +0900

    feat: 아이템 자석 기능, 아이템 및 재화 먹기

--- Assets/0.Scripts/System/Item/DroppedItem.cs
+++ Assets/0.Scripts/System/Item/DroppedItem.cs
@@ -6,26 +6,37 @@ using UnityEngine.Pool;
 /// </summary>
 public class DroppedItem : MonoBehaviour
 {
-    [HideInInspector] public ItemData ItemData;
-
-    [SerializeField] new private Rigidbody2D rigidbody2D;
+    public ItemData ItemData;
 
     public void Init(ItemData data, Vector3 dropPoint, Vector2 speedVector)
     {
         ItemData = data;
         gameObject.transform.position = dropPoint;
-        rigidbody2D.linearVelocity = speedVector;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (!other.CompareTag("Player")) return;
         Pickup();
     }
 
     private void Pickup()
     {
-        Player.Instance.inventory.AddItem(ItemData, 1);
-        print(ItemData);
+        if (ItemData.GetType() == typeof(MoneyItemData)) // 돈 아이템인 경우
+        {
+            MoneyItemData moneyData = ItemData as MoneyItemData;
+            if (moneyData != null)
+            {
+                MoneyManager.Instance.AddMoney(moneyData.amount);
+                Debug.Log("Money Picked up: " + moneyData.amount);
+            }
+        }
+        else // 일반 아이템인 경우
+        {
+            Player.Instance.inventory.AddItem(ItemData, 1);
+            Debug.Log("Item Picked up: " + ItemData.itemName);
+        }
+
 
         // DroppedItemManager를 통해 적절한 풀에 반환
         DroppedItemManager.Instance.ReturnToPool(this);
